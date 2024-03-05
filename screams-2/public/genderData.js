e8c6795b812028d5617611113f091bd4be6617a0
Perla Hoyos <perla.hoyosfuentes@stud.hslu.ch>
2024-03-05 13:48:48 +0100

    New Scream - 3

---
+++ screams-2/public/genderData.js
@@ -0,0 +1,12 @@
+export const genderData = {
+    FEMALE: {
+        gender: 'female',
+        sound: 'sounds/girl_scream.mp3', 
+        color: '#ffc0cb'
+    }, 
+    MALE: {
+        gender: 'male',
+        sound: 'sounds/male-scream.mp3',
+        color: '#1092c9',
+    }
+};
