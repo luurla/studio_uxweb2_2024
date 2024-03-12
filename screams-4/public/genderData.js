2ba80020ac70827310f915f8423f0bf385e69a70
luurla <perla.hoyosfuentes@stud.hslu.ch>
2024-03-12 10:59:49 +0100

    Hinzufügen von screams-4/node_modules/ zur .gitignore-Datei. Behebung von Binärdateiunterschieden in .DS_Store-Dateien.

---
+++ screams-4/public/genderData.js
@@ -0,0 +1,12 @@
+export const genderData = {
+    FEMALE: {
+        gender: 'female',
+        sound: 'sounds/girl-shout-2.mp3', 
+        color: '#DAF7A6 ',
+    }, 
+    MALE: {
+        gender: 'male',
+        sound: 'sounds/Scream-1-cartoon.mp3',
+        color: '#FECFB5',
+    }
+};
